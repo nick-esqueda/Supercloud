2f4fa65204b0c62bf2f9b948b8be6d475aa95ae7
nick-esqueda <nick_esqueda@outlook.com>
2022-03-03 12:16:15 -0600

    created auth functions and tested them with test routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,41 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-// ROUTES ***********************************************************
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+
+
+
+
+
+// // TEST ROUTES ***********************************************************
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//     where: { username: 'Demo User' }
+//   });
+
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// )
+
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// )
 
 module.exports = router;
