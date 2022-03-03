7f4742c37d7298758cb269492ff3e2e1827afb78
nick-esqueda <nick_esqueda@outlook.com>
2022-03-03 11:07:59 -0600

    created and tested server error handlers (except for sequelize error handler)

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// ROUTES ***********************************************************
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-
+// ROUTES ***********************************************************
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
