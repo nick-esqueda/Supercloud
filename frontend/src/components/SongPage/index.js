b37836f94e593956631bc6850e2ca9bab014faa5
nick-esqueda <nick_esqueda@outlook.com>
2022-03-06 00:09:46 -0600

    created skeleton layout for song page

--- frontend/src/components/SongPage/index.js
+++ frontend/src/components/SongPage/index.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import './SongPage.css'
 
 export default function SongPage() {
   return (
-    <div>SongPage</div>
+    <>
+      <div className='song_header'>
+        header
+      </div>
+
+      <div className='song_main'>
+        <div className='song__actions'>
+          actions
+        </div>
+
+        <div className='song__profile_card'>
+          profile card
+        </div>
+
+        <div className='song__body'>
+          <div className="song__description">
+            song description
+          </div>
+          <div className='song__comments'>
+            comment section
+          </div>
+        </div>
+
+        <div className='song_sidebar'>
+          sidebar
+        </div>
+        
+      </div>
+    </>
   )
 }
