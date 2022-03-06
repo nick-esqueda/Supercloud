5ecbe97029b8ae0b9f4050084c304c796dede801
nick-esqueda <nick_esqueda@outlook.com>
2022-03-06 13:41:46 -0600

    created songs slice of state (action, reducer), added reducer to combineReducers, put the song actions on the window

---
+++ frontend/src/store/songs.js
@@ -0,0 +1,36 @@
+// ACTION VARIABLES ***************************************
+const ADD_SONG = 'songs/ADD_SONG';
+
+
+// ACTION CREATORS ****************************************
+const addSong = (song) => {
+  return {
+    type: ADD_SONG,
+    song
+  }
+}
+
+
+
+// THUNK ACTION CREATORS **********************************
+
+
+
+
+
+// REDUCER ************************************************
+const initialState = { songs: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case ADD_SONG:
+      newState = {...state};
+      newState.songs[action.song.id] = action.song;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
