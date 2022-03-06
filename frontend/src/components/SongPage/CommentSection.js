24f0f09d82131f6ba4595115346acc5c308421b5
nick-esqueda <nick_esqueda@outlook.com>
2022-03-06 11:43:38 -0600

    positioned placeholders for comment section component

--- frontend/src/components/SongPage/CommentSection.js
+++ frontend/src/components/SongPage/CommentSection.js
@@ -3,6 +3,25 @@ import './CommentSection.css';
 
 export default function CommentSection() {
   return (
-    <div>CommentSection</div>
+    <div className='comment_section'>
+      
+      <span className='comments__comment_count'>💬 69 comments</span>
+      
+      <ul className='comments'>
+        {/* map through comments and render out comment component */}
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+        <li>comment placeholder</li>
+      </ul>
+      
+      
+    </div>
   )
 }
