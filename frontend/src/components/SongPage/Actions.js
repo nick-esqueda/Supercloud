e8842b1364618c5ce17131ebae95109762752925
nick-esqueda <nick_esqueda@outlook.com>
2022-03-07 20:57:02 -0600

    added elements to song actions component, small styles to some of the song page

--- frontend/src/components/SongPage/Actions.js
+++ frontend/src/components/SongPage/Actions.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './Actions.css';
 
 export default function Actions() {
   return (
-    <div>Actions</div>
+    <div className='actions_container'>
+      <div className='write_comment'>
+        <img src='https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+          alt='profile'
+        />
+
+        <form className="comment_form">
+          <div className="form_group" style={{ width: '100%' }}>
+            <input type="text"
+              id="comment"
+              placeholder='write a comment'
+              // value={title}
+              // onChange={e => setTitle(e.target.value)}
+              className="form_input"
+            // style={
+            //   showErrors && (validationErrors.includes('please enter a title'))
+            //     ? { borderColor: 'rgba(253, 69, 69, 0.829)' } : null
+            // }
+
+            />
+          </div>
+        </form>
+      </div>
+
+      <div className='bottom__buttons'>
+        <button className='btn btn--secondary--outline'>
+          <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGZpbGw9IiMzMzMiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTEwLjgwNSAzQzguNzg1IDMgOCA1LjM0NSA4IDUuMzQ1UzcuMjE0IDMgNS4xOTcgM0MzLjQ5NCAzIDEuNzQ4IDQuMDk2IDIuMDMgNi41MTRjLjM0NCAyLjk1MyA1LjcyNSA2LjQ4IDUuOTYzIDYuNDg3LjIzOC4wMSA1LjczOC0zLjcyIDUuOTg4LTYuNS4yMDgtMi4zLTEuNDczLTMuNS0zLjE3NS0zLjV6Ii8+Cjwvc3ZnPgo="
+            style={{ transform: 'scale(1.2)' }}
+          />
+          &nbsp;{'# of likes'}
+        </button>
+        <button className='btn btn--secondary--outline'>
+          <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHZpZXdCb3g9IjAgMCAxOCAxOCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGZpbGw9IiMzMzMiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTEyLjEgMTEuODZjLS44MjUtLjQxOC0xLjI0My0xLjUzNi0xLjI0My0yLjMyIDAtLjQuMjY4LS43MzUuNTA3LTEuMDA3LjY0OC0uNzQzIDEuMTU0LTEuNjI0IDEuMTU0LTMuNTA3QzEyLjUxOCAyLjI1IDEwLjg1OCAxIDguOTg4IDFjLTEuODcgMC0zLjUzIDEuMjUtMy41MyA0LjAyNiAwIDEuODgzLjUwNSAyLjc2NCAxLjE1MyAzLjUwNy4yNC4yNzIuNTEuNjA3LjUxIDEuMDA2IDAgLjc4NC0uNDIgMS45MDItMS4yNDYgMi4zMi0xLjI0NC42My0zLjQyMyAxLjE2Ny00LjM2NSAxLjg4Qy4yNSAxNC42OTUgMCAxNyAwIDE3aDE4cy0uMjc3LTIuMzA2LTEuNTM0LTMuMjZjLS45NDItLjcxMy0zLjEyLTEuMjUtNC4zNjUtMS44OHoiLz4KPC9zdmc+Cg=="
+            style={{ height: '16px' }}
+          />
+          &nbsp;
+          <NavLink to={`/users/`}>artist profile</NavLink>
+        </button>
+        <button className='btn btn--secondary--outline'>
+          <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHZpZXdCb3g9IjAgMCAxOCAxOCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9IiMzMzMiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTIgNmgxMHYxMEgyeiIvPgogICAgICAgIDxwYXRoIGZpbGwtb3BhY2l0eT0iLjciIGQ9Ik01IDJoMTF2MTBoLTJWNEg1eiIvPgogICAgPC9nPgo8L3N2Zz4K"
+            style={{ transform: 'scale(1.0)' }}
+          />
+          &nbsp;add to playlist
+        </button>
+
+        <button className='btn btn--secondary--outline'>
+          <img src="https://a-v2.sndcdn.com/assets/images/edit-2fe52d66.svg"
+            style={{ transform: 'scale(1.0)' }}
+          />
+          &nbsp;edit
+        </button>
+        <button className='btn btn--secondary--outline'>
+          <img src="https://a-v2.sndcdn.com/assets/images/delete-d90bf5e4.svg"
+            style={{ transform: 'scale(1.0)' }}
+          />
+          &nbsp;delete
+        </button>
+
+      </div>
+    </div>
   )
 }
