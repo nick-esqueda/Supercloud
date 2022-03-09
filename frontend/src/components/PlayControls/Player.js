ff14c042b4e99867dd86c5748c9be4a12761707a
nick-esqueda <nick_esqueda@outlook.com>
2022-03-09 03:13:29 -0600

    successfully tested editSong dispatch after 5 second play

--- frontend/src/components/PlayControls/Player.js
+++ frontend/src/components/PlayControls/Player.js
@@ -1,24 +1,26 @@
 import AudioPlayer from 'react-h5-audio-player';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useAudioPlayer } from '../../Context/AudioPlayerContext';
 import 'react-h5-audio-player/lib/styles.css';
 import { useEffect, useState } from 'react';
+import { editSong } from '../../store/songs';
 
 export default function Player() {
+  const dispatch = useDispatch();
   const song = useSelector(state => state.songs.playing);
   const { audioPlayer, paused, setPaused } = useAudioPlayer();
-  
   const [timer, setTimer] = useState('');
+  
 
   useEffect(() => {
-    console.log('CLEARING TIMER.');
     clearTimeout(timer);
   }, [paused]);
 
   const onPlay = () => {
     clearTimeout(timer);
     setTimer(setTimeout(() => {
-      console.log('TIMER DONE! HERE IS SONG TITLE...', song.title);
+      song.plays++;
+      dispatch(editSong(song));
     }, 5000))
 
     setPaused(false);
