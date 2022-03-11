afef8f18a123bc7467e282764497cba240989baf
nick-esqueda <nick_esqueda@outlook.com>
2022-03-11 12:43:44 -0600

    WIP styling song badge

--- frontend/src/components/SongBadge/SongBadge.js
+++ frontend/src/components/SongBadge/SongBadge.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import './SongBadge.css'
 
 export default function SongBadge() {
   return (
-    <div>SongBadge</div>
+    <div className='song_badge_container'>
+      <div className='song_badge__art'>
+        <img src='https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+          alt=''
+        />
+        <div className='hallway'></div>
+
+      </div>
+    </div>
   )
 }
