bedfb8daea0c38a0a9accdf2fb43d38fbdfffc21
nick-esqueda <nick_esqueda@outlook.com>
2022-03-10 18:53:30 -0600

    created comment card component with placholder text

--- frontend/src/components/CommentCard/index.js
+++ frontend/src/components/CommentCard/index.js
@@ -1,9 +1,22 @@
-
+import './CommentCard.css'
 
 export default function CommentCard() {
   return (
-    <div>
-      CommentCard
+    <div className='comment__wrapper'>
+      <div className='comment__left'>
+        <img src="https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+
+        <span style={{ color: '#b3b3b3' }}>username</span>
+        <span>comment content</span>
+      </div>
+      
+      <div className='comment__right'>
+        <span style={{ color: '#b3b3b3' }}>
+          time elapsed
+        </span>
+      </div>
     </div>
   )
 }
