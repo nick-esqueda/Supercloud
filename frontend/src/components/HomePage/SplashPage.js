9010ef199c822b2551c3277851cae422035e0fbb
nick-esqueda <nick_esqueda@outlook.com>
2022-03-12 18:28:35 -0600

    created splash page header, WIP buttons

--- frontend/src/components/HomePage/SplashPage.js
+++ frontend/src/components/HomePage/SplashPage.js
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import LoginModal from '../Modal/LoginModal';
+import SignupModal from '../Modal/SignupModal';
 import './SplashPage.css';
 
 export default function SplashPage() {
@@ -7,8 +9,27 @@ export default function SplashPage() {
   const user = useSelector(state => state.session.user);
 
   // if (user) history.push('/');
-  
+
   return (
-    <div>SplashPage</div>
+    <div className='splash_container'>
+      <div className='splash_header'>
+        <img className='splash_bg' src="https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt='gradient'
+        ></img>
+
+        <div className='splash__greeting'>
+          <span className='greeting__left'>looking for music? <br /> you're in the right place.</span>
+          <span className='greeting__right'>super<span style={{ textDecoration: 'overline', textDecorationColor: '#FFFF5d' }}>cloud</span></span>
+        </div>
+
+
+      </div>
+
+      <div className='splash_buttons'>
+        <LoginModal />
+        <SignupModal />
+      </div>
+
+    </div>
   )
 }
