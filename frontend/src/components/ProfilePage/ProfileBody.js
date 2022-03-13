d395be1f0f84b3987b3377e62fc65d93f6294267
nick-esqueda <nick_esqueda@outlook.com>
2022-03-13 05:14:46 -0500

    set up profile page header, set framework for tabs

---
+++ frontend/src/components/ProfilePage/ProfileBody.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './ProfileBody.css';
+
+export default function ProfileBody() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  return (
+    <div className='profile_body_container'>
+      <div className='profile__tabs'>
+        <div
+          className={activeTab === 1 ? 'tab active_tab' : 'tab'}
+          onClick={() => setActiveTab(1)}
+        >songs</div>
+        <div
+          className={activeTab === 2 ? 'tab active_tab' : 'tab'}
+          onClick={() => setActiveTab(2)}
+        >likes</div>
+        <div
+          className={activeTab === 3 ? 'tab active_tab' : 'tab'}
+          onClick={() => setActiveTab(3)}
+        >cements</div>
+      </div>
+      
+      <div 
+        className={activeTab === 1 ? 'active content songs_tab' : 'inactive content songs_tab'}
+      >
+        songsaksfhoawiefne
+      </div>
+      <div 
+        className={activeTab === 2 ? 'active content likes_tab' : 'inactive content likes_tab'}
+      >
+        liwkemfklkfan/lsf s
+      </div>
+      <div 
+        className={activeTab === 3 ? 'active content comments_tab' : 'inactive content comments_tab'}
+      >
+        CEAMENTe
+      </div>
+    </div>
+  )
+}
--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,7 +1,44 @@
+import { useSelector } from 'react-redux';
+import ProfileBody from './ProfileBody';
 import './ProfilePage.css';
 
 export default function ProfilePage() {
+  // need to fetch the "artist" using the artist thunk to get the user's info
+  const user = useSelector(state => state.session.user);
+
   return (
-    <div>ProfilePage</div>
+    <div id='profile_page'>
+      <div className="profile__header">
+        <div className="profile_header__banner">
+          <img src={user?.bannerImageURL
+            ? user?.bannerImageURL
+            : "https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+            alt="gradient"
+          />
+        </div>
+
+        <div className='user_info_container'>
+          <img src={user.profileImageURL
+            ? user.profileImageURL
+            : "https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          }
+            alt='profile'
+          />
+          <div className='profile__info'>
+            <h2>{user.username}</h2>
+            <span style={{ fontStyle: 'italic' }}>{user.location ? null : 'nashville cocknuts'}</span>
+            <p>{'Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non'}</p>
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className='profile__body'>
+        <ProfileBody />
+      </div>
+    </div>
   )
 }
