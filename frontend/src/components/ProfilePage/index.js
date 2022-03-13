562227c34a5a44e2ab115ccecf429d5bfd61b977
nick-esqueda <nick_esqueda@outlook.com>
2022-03-13 10:00:44 -0500

    made sure that fetchArtist isn't called whenever session user is looking at his own profile

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,6 +1,6 @@
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 import { fetchArtist } from '../../store/artists';
@@ -11,20 +11,26 @@ import './ProfilePage.css';
 export default function ProfilePage() {
   const dispatch = useDispatch();
   const { userId } = useParams();
-  const user = useSelector(state => state.artists)[userId];
+  const sessionUser = useSelector(state => state.session.user);
+  const [artist, setArtist] = useState('');
   
   useEffect(() => {
-    (async () => {
-      await dispatch(fetchArtist(userId));
-    })()
+    if (sessionUser.id === userId) {
+      setArtist(sessionUser);
+    } else {
+      (async () => {
+        const artist = await dispatch(fetchArtist(userId));
+        setArtist(artist);
+      })()
+    }
   }, [dispatch]);
 
-  return !user ? <h2>loading...</h2> : (
+  return !artist ? <h2>loading...</h2> : (
     <div id='profile_page'>
       <div className="profile__header">
         <div className="profile_header__banner">
-          <img src={user.bannerImageURL
-            ? user.bannerImageURL
+          <img src={artist.bannerImageURL
+            ? artist.bannerImageURL
             : "https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
           }
             alt="gradient"
@@ -32,15 +38,15 @@ export default function ProfilePage() {
         </div>
 
         <div className='user_info_container'>
-          <img src={user.profileImageURL
-            ? user.profileImageURL
+          <img src={artist.profileImageURL
+            ? artist.profileImageURL
             : "https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGdyYWRpZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
           }
             alt='profile'
           />
           <div className='profile__info'>
-            <h2>{user.username}</h2>
-            {user.location && (<span style={{ fontStyle: 'italic' }}>{user.location}</span>)}
+            <h2>{artist.username}</h2>
+            {artist.location && (<span style={{ fontStyle: 'italic' }}>{artist.location}</span>)}
             <p>{'Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non'}</p>
           </div>
         </div>
@@ -51,7 +57,7 @@ export default function ProfilePage() {
         <ProfileBody />
       </div>
 
-      {/* <div className='profile__sidebar'>
+      <div className='profile__sidebar'>
         <div className='sidebar_container'>
           <h4 className='flexRowBetween'>
             <div className='alignItems'>
@@ -59,16 +65,16 @@ export default function ProfilePage() {
               &nbsp;songs you liked
             </div>
 
-            <NavLink to={`/users/${user.id}/likes`} className="italic">view all</NavLink>
+            <NavLink to={`/users/${artist.id}/likes`} className="italic">view all</NavLink>
           </h4>
 
           <ul className="songs">
-            {user.Likes.slice(0,3).map((song, i) => (
+            {artist.Likes.slice(0,3).map((song, i) => (
               <li key={i}><SongCardSmall song={song} /></li>
             ))}
           </ul>
         </div>
-      </div> */}
+      </div>
 
     </div>
   )
