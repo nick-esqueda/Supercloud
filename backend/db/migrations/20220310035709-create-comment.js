1b10536f4f6009c50ce2d74b1a4a8e99b8eead2f
nick-esqueda <nick_esqueda@outlook.com>
2022-03-14 11:53:09 -0500

    I HOPE I FIXED IT

--- backend/db/migrations/20220310035709-create-comment.js
+++ backend/db/migrations/20220310035709-create-comment.js
@@ -11,12 +11,14 @@ module.exports = {
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'Users' }
+        references: { model: 'Users' },
+        onDelete: "CASCADE"
       },
       songId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'Songs' }
+        references: { model: 'Songs' },
+        onDelete: "CASCADE"
       },
       content: {
         allowNull: false,
