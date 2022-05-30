9658eef76249d99f4396c9dba57e3fb4dc5f8181
nick-esqueda <nick_esqueda@outlook.com>
2022-05-30 14:02:42 -0500

    WIP player refactor

--- frontend/src/components/Footer/Player.js
+++ frontend/src/components/Footer/Player.js
@@ -5,18 +5,35 @@ import { useEffect, useState } from 'react';
 import { editSong, editSongPlays } from '../../store/songs';
 import './Player.css';
 
+function amplifyMedia(mediaElem, multiplier) {
+  var context = new (window.AudioContext || window.webkitAudioContext),
+      result = {
+        context: context,
+        source: context.createMediaElementSource(mediaElem),
+        gain: context.createGain(),
+        media: mediaElem,
+        amplify: function(multiplier) { result.gain.gain.value = multiplier; },
+        getAmpLevel: function() { return result.gain.gain.value; }
+      };
+  result.source.connect(result.gain);
+  result.gain.connect(context.destination);
+  result.amplify(multiplier);
+  return result;
+}
+
 export default function Player() {
   const dispatch = useDispatch();
   const song = useSelector(state => state.songs.playing);
   const { audioPlayer, paused, setPaused } = useAudioPlayer();
   const [timer, setTimer] = useState('');
   
-
   useEffect(() => {
     clearTimeout(timer);
   }, [paused]);
-
+  
   const onPlay = () => {
+    audioPlayer.current.audio.current.crossOrigin = "anonymous";
+    amplifyMedia(audioPlayer.current.audio.current, .5);
     clearTimeout(timer);
     setTimer(setTimeout(() => {
       song.plays++;
@@ -35,6 +52,7 @@ export default function Player() {
     <>
       <AudioPlayer
         autoPlay
+        id={'bruh'}
         src={song?.songURL}
         onPlay={e => onPlay()}
         onPause={e => onPause()}
